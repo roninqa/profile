3fbb3e28cd56609f33ab92edcfa34bbae8cea6f5
cher1 <chue.her@autotrader.com>
2017-04-13 06:26:50 -0400

    b/c i'm happy

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import "html/template"
+import "net/http"
+import "log"
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(res http.ResponseWriter, req *http.Request) {
+	err := tpl.ExecuteTemplate(res, "index.gohtml", nil)
+	if err != nil {
+		log.Fatalln("template didn't execute: ", err)
+	}
+}
